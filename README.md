e19582227f4e916b7360bec72381ce188ca12826
Aidan Daniel <aidan.d@wearedrum.com>
2021-07-16 21:05:08 +0100

    Initial commit
    Pulled in library, starting with simple mp4 to gif video

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# clipper
+# portscape
+
+## A Vue / FFmpeg.wasm app to convert landscape video to portrait for use on TikTok or YouTube #shorts.
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <input type="file" @change="setVideo($event)">
+
+        <div v-if="videoFile">
+            <video :src="videoFile" width="250" controls></video>
+            <button @click="convertToGif">Convert to gif</button>
+        </div>
+
+        <div v-if="gif">
+            <h3>Result</h3>
+            <img :src="gif" alt="" width="250">
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// @ is an alias to /src ... so can't use @ffmpeg without absolute path?
+import { createFFmpeg, fetchFile } from "../../node_modules/@ffmpeg/ffmpeg";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+    name: "Home",
+    data: function () {
+        return {
+            ffmpeg: null,
+            videoFile: null,
+            gif: null,
+        };
+    },
+    created() {
+        this.ffmpeg = createFFmpeg({
+            log: true,
+        });
+        // Initialise loadFFmpeg
+        this.loadFFmpeg();
+    },
+    methods: {
+        async loadFFmpeg() {
+            await this.ffmpeg.load();
+        },
+        setVideo(e) {
+            if (e.target.files.length > 0) {
+                this.videoFile = URL.createObjectURL(e.target.files?.item(0));
+            }
+        },
+        async convertToGif() {
+            // Write file to memory
+            this.ffmpeg.FS("writeFile", "inputFile.mp4", await fetchFile(this.videoFile));
+
+            // Run the FFMpeg command
+            // i = input file, t = length of output, -ss (starting seconds) offset, -f = encode as ...
+            await this.ffmpeg.run("-i", "inputFile.mp4", "-t", "2.5", "-ss", "2.0", "-f", "gif", "outputFile.gif");
+
+            // Read the result
+            const data = this.ffmpeg.FS("readFile", "outputFile.gif");
+
+            // Create a URL
+            const url = URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
+
+            // Update data value
+            this.gif = url;
+        },
+    },
 };
 </script>
