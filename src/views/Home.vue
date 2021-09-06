5f35a56ce45220b4400ffc15f74b40faf1d687cc
Aidan Daniel <aidan.d@wearedrum.com>
2021-09-06 08:16:54 +0100

    Applying crop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -87,38 +87,42 @@ export default {
             var cropperData = cropper.getData(!0);
             console.log(cropperData);
 
-            // // Write file to memory
-            // this.ffmpeg.FS("writeFile", "input.mp4", await fetchFile(this.videoSrcFile));
+            // Write file to memory
+            this.ffmpeg.FS("writeFile", "input.mp4", await fetchFile(this.videoSrcFile));
+            var n = this.$refs.videoSrcEl.videoHeight;
+            var a = this.$refs.videoSrcEl.videoWidth;
+            var r = Math.round(n / 1.77);
+            var s = Math.round(a / 2);
 
-            // // Templates:
+            // Templates:
 
-            // // Blurred background - crops video to a rectangle and makes abckground blurred version of the video.
-            // // crop=output_w:output_h:x:y
-            // var blurred = [
-            //     "-i",
-            //     "input.mp4",
-            //     "-c:v",
-            //     "libx264",
-            //     "-filter_complex",
-            //     "[0:v]crop=".concat(r, ":").concat(n, ":").concat(s, ":0[croppedbg];[croppedbg]scale=720:1280,boxblur=10[background];[0:v]crop=").concat(cropperData.width, ":").concat(cropperData.height, ":").concat(cropperData.x, ":").concat(cropperData.y, "[croppedgf];[croppedgf]scale=720:-2[gameplay];[background][gameplay]overlay=y=(H-h)/2"),
-            //     "-c:a",
-            //     "copy",
-            //     "output.mp4",
-            // ];
+            // Blurred background - crops video to a rectangle and makes abckground blurred version of the video.
+            // crop=output_w:output_h:x:y
+            var blurred = [
+                "-i",
+                "input.mp4",
+                "-c:v",
+                "libx264",
+                "-filter_complex",
+                "[0:v]crop=".concat(r, ":").concat(n, ":").concat(s, ":0[croppedbg];[croppedbg]scale=720:1280,boxblur=10[background];[0:v]crop=").concat(cropperData.width, ":").concat(cropperData.height, ":").concat(cropperData.x, ":").concat(cropperData.y, "[croppedgf];[croppedgf]scale=720:-2[gameplay];[background][gameplay]overlay=y=(H-h)/2"),
+                "-c:a",
+                "copy",
+                "output.mp4",
+            ];
 
-            // // // Split - Puts the face cam video on top
-            // // var split = ["-i", "input.mp4", "-c:v", "libx264", "-filter_complex", "[0:v]crop=385:216:448:252[croppedfc];[croppedfc]scale=720:-2[top];[0:v]crop=591:720:344:0[croppedgf];[croppedgf]scale=720:-2[bottom];[top][bottom]vstack", "-c:a", "copy", "output.mp4"];
+            // // Split - Puts the face cam video on top
+            // var split = ["-i", "input.mp4", "-c:v", "libx264", "-filter_complex", "[0:v]crop=385:216:448:252[croppedfc];[croppedfc]scale=720:-2[top];[0:v]crop=591:720:344:0[croppedgf];[croppedgf]scale=720:-2[bottom];[top][bottom]vstack", "-c:a", "copy", "output.mp4"];
 
-            // await this.ffmpeg.run(...blurred);
+            await this.ffmpeg.run(...blurred);
 
-            // // Read the result
-            // const data = this.ffmpeg.FS("readFile", "output.mp4");
+            // Read the result
+            const data = this.ffmpeg.FS("readFile", "output.mp4");
 
-            // // Create a URL
-            // const url = URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }));
+            // Create a URL
+            const url = URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }));
 
-            // // Update data value
-            // this.croppedVideo = url;
+            // Update data value
+            this.croppedVideo = url;
         },
     },
 };
